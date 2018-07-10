087e90986b4eb7fb4824ce62083d00ca977b5951
knromaric <nzekeng@gmail.com>
2018-07-10 18:24:48 +0300

    initial commit - Training rooms demo

---
+++ TrainingRooms/TrainingRooms/RoomRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainingRooms
+{
+    public class RoomRepository
+    {
+        private List<TrainingRoom> _rooms = new List<TrainingRoom>
+        {
+            new TrainingRoom{
+                Id = 1,
+                Name = "Copernicus",
+                Location = "bldg 1",
+                NumberComputers = 25
+            },
+            new TrainingRoom{
+                Id = 2,
+                Name = "Zirka",
+                Location = "bldg 2",
+                NumberComputers = 20
+            },
+            new TrainingRoom{
+                Id = 3,
+                Name = "Hewet",
+                Location = "bldg 1",
+                NumberComputers = 40
+            },
+            new TrainingRoom{
+                Id = 4,
+                Name = "Fikra",
+                Location = "bldg 3",
+                NumberComputers = 37
+            }
+        };
+        public List<TrainingRoom> GetTrainingRooms()
+        {
+            return _rooms;
+        }
+
+        public TrainingRoom GetRoom(int id)
+        {
+            return _rooms.Where(r => r.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ TrainingRooms/trainingrooms.droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using TrainingRooms;
+using Android.Views;
+using Android.Content;
+
+namespace trainingrooms.droid
+{
+    [Activity(Label = "trainingrooms.droid", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var repo = new RoomRepository();
+            var rooms = repo.GetTrainingRooms();
+            var adapter = new ArrayAdapter<TrainingRoom>(this, Resource.Layout.RoomListItem, rooms.ToArray());
+            this.ListAdapter = adapter;
+        }
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            var intent = new Intent(this, typeof(TrainingRoomDetailActivity));
+            var selectedItem = ((ArrayAdapter<TrainingRoom>)ListAdapter).GetItem(position);
+            intent.PutExtra("roomId", selectedItem.Id);
+            StartActivity(intent);
+        }
+    }
+}
+
