970d90513c26c256906a7031b9baddac5e39a3f0
knromaric <nzekeng@gmail.com>
2018-09-04 15:55:42 +0300

    LayoutDemo apps

---
+++ LayoutDemoApp/LayoutDemoApp/LayoutDemoApp/StackLayoutDemo2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutDemoApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StackLayout_insta : ContentPage
+	{
+		public StackLayout_insta ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
