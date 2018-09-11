d2857effb8e3458ef10e449f780a2424cd097614
knromaric <nzekeng@gmail.com>
2018-09-11 11:21:03 +0300

    Netflix Roulette App

---
+++ NetflixRouletApp/NetflixRouletApp/NetflixRouletApp/App.xaml.cs
@@ -0,0 +1,33 @@
+using NetflixRouletApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace NetflixRouletApp
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new MoviesPage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ NetflixRouletApp/NetflixRouletApp/NetflixRouletApp/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetflixRouletApp.Models
+{
+    public class Movie
+    {
+        [JsonProperty("show_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("show_title")]
+        public string Title { get; set; }
+
+        [JsonProperty("release_year")]
+        public int ReleaseYear { get; set; }
+
+        [JsonProperty("poster")]
+        public string Poster { get; set; }
+
+        [JsonProperty("rating")]
+        public float Rating { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+
+    }
+}
